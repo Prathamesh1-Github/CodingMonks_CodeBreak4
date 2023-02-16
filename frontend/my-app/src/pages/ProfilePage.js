a6bf23ec65fa7ebb92e11fef6696613e65805040
Prathamesh Dhorage <prathameshdhorage@gmail.com>
2023-02-16 05:56:19 +0530

    worked on the filters section and done some more changes

--- frontend/my-app/src/pages/ProfilePage.js
+++ frontend/my-app/src/pages/ProfilePage.js
@@ -8,11 +8,11 @@ const ProfilePage = () => {
     const [isError, setIsError] = useState('')
 
     const getProfileInfo = async () => {
-        try{
+        try {
             const res = await axios.get(
                 "http://localhost:3000/api/v1/profile",
                 {
-                    headers:{
+                    headers: {
                         "Authorization": "Bearer " + localStorage.getItem('token')
                     }
                 }
@@ -20,7 +20,7 @@ const ProfilePage = () => {
             console.log(res.data)
             setProfileInfo(res.data)
         }
-        catch(error){
+        catch (error) {
             setIsError(error)
         }
     }
@@ -30,14 +30,59 @@ const ProfilePage = () => {
     useEffect(() => {
         getProfileInfo()
     }, [])
-
+    
     return (
         <div className="mainPage">
-            <MainNavbar/>
+            <MainNavbar />
             {/* <h1>{profileInfo[0]}</h1> */}
             {
                 profileInfo.map((curEle) => {
-                    return <h1>Name: {curEle.name} Age: {curEle.age} MobileNo : {curEle.mobileNo}</h1>
+                    return (
+                        <div>
+                            <div className="container-fluid mycont">
+
+                                <div className="row">
+                                    <div className="col-lg-4 text-center custcard1">
+                                        <img src="images/balnk_profile_img.webp" style="max-width: 10rem;max-height: 10rem;border-radius: 100%;"
+                                            alt="profilepic">
+                                    </div>
+                                    <div className="col-lg-1">
+                                        <p></p>
+                                    </div>
+                                    <div className="col-lg-7 align-self-center pdl2 custcard">
+                                        <h2>Dhruv Kshirasagar</h2>
+                                        <h5 className="ogloc"><i className="fa-solid fa-location-dot"></i><span className="myloc"> Pune, Maharashtra</span>
+                                        </h5>
+                                        <br>
+                                            <p>Ratings : ⭐⭐⭐⭐⭐</p>
+                                            <div className="row prowcol" >
+                                                <div className="col-lg-6 ">
+                                                    <p>Phone Number</p>
+                                                </div>
+                                                <div className="col-lg-6">
+                                                    <p>+91 8999080125</p>
+                                                </div>
+                                                <div className="col-lg-6 ">
+                                                    <p>E-mail</p>
+                                                </div>
+                                                <div className="col-lg-6">
+                                                    <p>dhruv@gmail.com</p>
+                                                </div>
+                                                <div className="col-lg-6 ">
+                                                    <p>Age</p>
+                                                </div>
+                                                <div className="col-lg-6">
+                                                    <p>21</p>
+                                                </div>
+                                            </div>
+                                    </div>
+
+                                </div>
+                                <hr>
+                            </div>
+
+                        </div>
+                    )
                 })
             }
         </div>
--- frontend/my-app/src/pages/SearchPage.js
+++ frontend/my-app/src/pages/SearchPage.js
@@ -1,74 +1,106 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 import RideCard from "../components/RideCard"
 import MainNavbar from "../components/MainNavbar"
 import "../styles/searchpage.css"
+import { Select } from '@mui/material'
 // import LocationOnIcon from '@mui/icons-material/LocationOn';
+
+import { FormControl, InputLabel, MenuItem } from '@mui/material';
 
 function SearchPage() {
     const [startlocation, setstartlocation] = useState('')
     const [destination, setDestination] = useState('')
+
+    const [price, setPrice] = useState('')
+    const [date, setDate] = useState('')
     // const [date, setDate] = useState('')
     const [rideData, setRideData] = useState({})
 
-    function searchForPlayer(event){
+    function searchForPlayer(event) {
         event.preventDefault()
-        var APICallString = `http://localhost:3000/api/v1/rides/all/userrides?startLocation=${startlocation}&destination=${destination}`
+        var APICallString = `http://localhost:3000/api/v1/rides/all/userrides?startLocation=${startlocation}&destination=${destination}&sort=${price},${date}`
         axios.get(APICallString,
-                {
-                    headers: {
-                        "Authorization": "Bearer " + localStorage.getItem('token')
-                    }
+            {
+                headers: {
+                    "Authorization": "Bearer " + localStorage.getItem('token')
                 }
-            ).then(function(response){
+            }
+        ).then(function (response) {
             setRideData(response.data)
-        }).catch(function(error){
+        }).catch(function (error) {
             console.log(error)
         })
     }
 
     console.log(rideData)
 
-    return(
+    return (
         <div>
-            <MainNavbar/>
+            <MainNavbar />
             <div className='searchSection'>
                 <div className='searchbox'>
                     {/* <h5>startlocation</h5> */}
-                    <input className='search-input1'  type="text" onChange={e => setstartlocation(e.target.value)} placeholder="Leaving from..."/>
+                    <input className='search-input1' type="text" onChange={e => setstartlocation(e.target.value)} placeholder="Leaving from..." />
                     {/* <h5>destination</h5> */}
-                    <input className='search-input2' type="text" onChange={e => setDestination(e.target.value)} placeholder="Going to..."/>
-                    {/* <h5>date</h5>
-                    <input type="date" onChange={e => setDate(e.target.value)}/> */}
+                    <input className='search-input2' type="text" onChange={e => setDestination(e.target.value)} placeholder="Going to..." />
+                    {/* <h5>date</h5> */}
                 </div>
                 <button className='search-button' onClick={e => searchForPlayer(e)}>Search</button>
             </div>
-            {JSON.stringify(rideData) !== '{}' ? 
-            <>
-                {rideData.map((ride) => {
-                    const {startLocation, destination, date, time, car, carNumber, prefferedLoad, price, createdBy, createdByName, _id} = ride
-                    return(
-                        <RideCard
-                            key = {_id}
-                            startLocation = {startLocation}
-                            destination = {destination}
-                            date = {date}
-                            time = {time}
-                            car = {car}
-                            carNumber = {carNumber}
-                            prefferedLoad = {prefferedLoad}
-                            price = {price}
-                            createdBy = {createdBy}
-                            createdByName = {createdByName}
-                            _id = {_id}
-                        />
-                    )
-                })}
-            </> 
-            : 
-            <>
-                {/* We dont have player data */}
-            </>
+            <div className='searchSectionFilter'>
+                    {/* <input className='search-input1' type="text" onChange={e => setPrice(e.target.value)} placeholder="price" /> */}
+                    <FormControl fullWidth>
+                        <InputLabel id="demo-simple-select-label">Price</InputLabel>
+                        <Select
+                            className='search-input1'
+                            label="Price Filter"
+                            onChange={e => setPrice(e.target.value)}
+                        >
+                            <MenuItem value="price">Low To High</MenuItem>
+                            <MenuItem value="-price">High To Low</MenuItem>
+                        </Select>
+                    </FormControl>
+                    <FormControl fullWidth>
+                        <InputLabel id="demo-simple-select-label">Date</InputLabel>
+                        <Select
+                            className='search-input1'
+                            label="Price Filter"
+                            onChange={e => setDate(e.target.value)}
+                        >
+                            <MenuItem value="date">Most Recent</MenuItem>
+                            <MenuItem value="-date">Recent</MenuItem>
+                        </Select>
+                    </FormControl>
+                <button className='search-button-filter' onClick={e => searchForPlayer(e)}>Apply</button>
+                    {/* <input className='search-input2' type="text" onChange={e => setDate(e.target.value)} placeholder="date" /> */}
+            </div>
+            {JSON.stringify(rideData) !== '{}' ?
+                <>
+                    {rideData.map((ride) => {
+                        const { startLocation, destination, date, time, car, carNumber, prefferedLoad, price, createdBy, createdByName, _id } = ride
+                        return (
+                            <RideCard
+                                key={_id}
+                                startLocation={startLocation}
+                                destination={destination}
+                                date={date}
+                                time={time}
+                                car={car}
+                                carNumber={carNumber}
+                                prefferedLoad={prefferedLoad}
+                                price={price}
+                                createdBy={createdBy}
+                                createdByName={createdByName}
+                                _id={_id}
+                            />
+                        )
+                    })}
+                </>
+                :
+                <>
+                    {/* We dont have player data */}
+                </>
             }
         </div>
     )
